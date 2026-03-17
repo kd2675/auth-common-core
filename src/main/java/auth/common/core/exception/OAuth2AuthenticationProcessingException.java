b0b4ffd75e8b8110c1ea09aa372c33f6cf9a0111
김도영 <zz01064842675@gmail.com>
2026-03-17 15:28:09 +0900

    feat: 카카오 로그인 및 에러 페이지 설정

--- src/main/java/auth/common/core/exception/OAuth2AuthenticationProcessingException.java
+++ src/main/java/auth/common/core/exception/OAuth2AuthenticationProcessingException.java
@@ -1,11 +1,37 @@
 package auth.common.core.exception;
 
 public class OAuth2AuthenticationProcessingException extends RuntimeException {
+    private final String errorCode;
+    private final String providerHint;
+
     public OAuth2AuthenticationProcessingException(String msg, Throwable t) {
-        super(msg, t);
+        this(msg, t, null, null);
     }
 
     public OAuth2AuthenticationProcessingException(String msg) {
-        super(msg);
+        this(msg, null, null, null);
+    }
+
+    public OAuth2AuthenticationProcessingException(String msg, String errorCode, String providerHint) {
+        this(msg, null, errorCode, providerHint);
+    }
+
+    public OAuth2AuthenticationProcessingException(
+            String msg,
+            Throwable t,
+            String errorCode,
+            String providerHint
+    ) {
+        super(msg, t);
+        this.errorCode = errorCode;
+        this.providerHint = providerHint;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public String getProviderHint() {
+        return providerHint;
     }
 }
